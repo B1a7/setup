b2ce349a4d9028a6143f43fc08a00bca1bb42c94
B1a7 <93325616+B1a7@users.noreply.github.com>
2023-05-09 10:42:52 +0200

    style: changes in Program.cs

--- Program.cs
+++ Program.cs
@@ -14,15 +14,12 @@ namespace setup
         [DllImport("user32.dll")]
         static extern bool ShowWindow(IntPtr hWnd, int nCmdShow);
         const int SW_HIDE = 0;
-        const int SW_SHOW = 5;
 
         private static void Main(string[] args)
         {
+            //Hide console window
             var handle = GetConsoleWindow();
-
-            // Hide
             ShowWindow(handle, SW_HIDE);
-
 
             if (!IsAdministrator())
             {
